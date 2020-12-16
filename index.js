d6c779a356ef6843792522fccf55ef0065819bc8
Ivan Huang <huangiw@uci.edu>
2020-12-16 02:43:36 -0800

    Added cors

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const package = require('./package.json');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
+app.use(cors())
 app.use('/api/search', require('./api/search.js'));
-
 app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
