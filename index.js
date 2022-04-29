4c91033878dee3e3458c5a53202633e0a1a19f6c
Pundora <roy.jou@pundora.org>
2022-04-28 23:37:45 -0700

    Refactor backend API index.js

--- index.js
+++ index.js
@@ -1,32 +1,50 @@
-const express = require('express');
+/* API Initialization */
 const axios = require('axios');
+console.log(`\nLoading GrumbleJumble API...`);
+global.SERVER = {};
+
+/* API Path */
+SERVER.path = __dirname;
+
+/* Initialize Express Server */
+const express = require('express');
+SERVER.express = express();
+
+/* HTTPS */
+const fs = require('fs');
+const https = require('https');
+
+/* CORS Configuration */
 const cors = require('cors');
+const originWhitelist = [
+  'https://localhost:8000',
+  'https://localhost:5000',
+  'https://localhost:3000',
+  'https://grumblejumble.com'
+]
+const corsOptions = { origin: function (origin, callback) { if (originWhitelist.indexOf(origin) !== -1) { callback(null, true); } else { callback(null, false); }}}
+SERVER.express.use(cors(corsOptions));
 
-const package = require('./package.json');
+/* V1 API (Deprecated) */
+SERVER.express.use('/v1', require('./api/v1/api.js'));
 
-const app = express();
+/* V2 API (Current) */
+SERVER.express.use('/v2', require('./api/v2/api.js'));
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
+/* Reject all other request routes */
+SERVER.express.use('*', (req, res) => { res.status(400).end(); });
 
-app.use(cors());
-app.use('/api/search', require('./api/search.js'));
-app.use(express.json({ extended: false }));
+/* API Options */
+SERVER.express.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-let server = app.listen(PORT, () => {
+let server = SERVER.express.listen(PORT, () => {
 
   console.log('\n====================================');
-  console.log(`GrumbleJumble API v${package.version}`);
-  console.log(``);
-  console.log(`Secure REST API Endpoint:`);
-  console.log(`http://grumblejumble.herokuapp.com:${server.address().port}`);
+  console.log(`GrumbleJumble API Online`);
+  console.log(`Current Server Time: ${new Date()}`);
+  console.log(`http://api.grumblejumble.com:${server.address().port}`);
   console.log(`====================================\n`);
 
 });
