6f51a680c5699444ad9a07f4e17d5019060bedbe
Pundora <roy.jou@pundora.org>
2022-05-10 00:23:58 -0700

    Update API Version 2 with new endpoints

--- api/v2/api.js
+++ api/v2/api.js
@@ -4,8 +4,84 @@ const axios = require('axios');
 const router = express.Router();
 
 const baseURL = "https://api.yelp.com/v3/businesses/search";
-const apikey = process.env.YELP_API_KEY;
+const apiKey = process.env.YELP_API_KEY || require(`${SERVER.path}/api.json`).apikey;
+const reg = /-?([0-9]+).([0-9]+)/g;
 
-router.get('/', (req, res) => { res.status(200).end(); });
+/* Optional Parameters */
+const DEFAULT = {
+  term: "restaurant",
+  openNow: true,
+  radius: 5
+};
+
+const endpointInformation = {
+  random: {
+    requiredParameters: {
+      'lat': 'Standard ISO6709 +- Decimal Representation of Latitude',
+      'long': 'Standard ISO6709 +- Decimal Representation of Longitude'
+    },
+    optionalParameters: {
+      'term': 'Yelp API Term for Restaurant, Defaults to "Restaurant"',
+      'openNow': 'True/False, Defaults to True',
+      'radius': 'Integer in Miles, Defaults to 5'
+    }
+  }
+}
+
+
+router.get('/', (req, res) => { /* Available Endpoints */ return res.status(200).json(endpointInformation); });
+
+router.get('/random', async (req, res) => {
+
+  /* Check if required parameters have been left blank */
+  if (typeof req.query.lat === 'undefined') { return res.status(400).send('Parameter: LATITUDE cannot be left blank'); }
+  if (typeof req.query.long === 'undefined') { return res.status(400).send('Parameter: LONGITUDE cannot be left blank'); }
+
+  /* Assign parameters to variables, using defaults as necessary for optional parameters */
+  let latitude = req.query.lat;
+  let longitude = req.query.long;
+  let term = req.query.term || DEFAULT.term;
+  let openNow = req.query.openNow || DEFAULT.openNow;
+  let radius = req.query.radius || DEFAULT.radius; radius *= 1609;
+
+  /* Check for incorrectly formatted variables */
+  if (!(latitude.match(reg))) { return res.status(400).send('Invalid Parameter: LATITUDE'); }
+  if (!(longitude.match(reg))) { return res.status(400).send('Invalid Parameter: LONGITUDE'); }
+
+  /* Construct yelp request and send to Yelp API */
+  let yelpRequest = `${baseURL}?term=${term}&radius=${radius}&open_now=${openNow}&latitude=${latitude}&longitude=${longitude}`;
+  let yelpResponse = await axios.get(yelpRequest, { headers: { 'Authorization': `Bearer ${apiKey}` }}).catch(err => {
+    console.log(err); res.status(500).end();
+  });
+
+  /* Select random business */
+  let randIndex = Math.floor(Math.random() * yelpResponse.data.businesses.length);
+  let business = yelpResponse.data.businesses[randIndex];
+
+  /* Make yelp response data human readable */
+  let businessAddress = `${business.location.display_address.join(", ")}`;
+  let destinationCoordinates = `${business.coordinates.latitude},${business.coordinates.longitude}`;
+  let originCoordinates = `${latitude},${longitude}`;
+  let mapsURL = `https://www.google.com/maps/dir/?api=1&origin=${originCoordinates}&destination=${destinationCoordinates}`;
+
+  /* Pipe results into GrumbleJumble frontend format */
+  var data = new Object();
+  data.name = business.name;
+  data.image_url = business.image_url;
+  data.category = business.categories[0].title;
+  data.rating = business.rating;
+  data.price = business.price;
+  data.address = businessAddress;
+  data.addressLink = mapsURL;
+  data.yelpLink = business.url;
+  data.phone = business.display_phone;
+  data.distance = metersToMiles(business.distance);
+
+  /* Return random business */
+  res.status(200).json(data);
+
+});
+
+function metersToMiles(m) { return `${Math.round(m / 1609 * 100) / 100} mi.`; }
 
 module.exports = router;
